7460275f2f67b3b7195543351d32a012d9d1aac2
Emmet <emmetlawton@gmail.com>
2019-12-23 19:40:02 +0000

    workign with strings

---
+++ strings1.rb
@@ -0,0 +1,38 @@
+first_name = "Emmet"
+last_name = "Matthews"
+
+puts first_name + " " + last_name
+#works 
+puts "My first name is #{first_name} and my last name is #{last_name}"
+
+#doesn't work
+puts 'My first name is #{first_name} and my last name is #{last_name}'
+
+# primitive type class
+puts first_name.class
+
+#all methods available
+#puts
+first_name.methods()
+
+#reverse string
+puts first_name.reverse()
+
+#empty check
+puts first_name.empty?
+puts "".empty?
+
+#change words in sentence
+sentence = "Welcome to The Jungle"
+puts sentence.sub("The Jungle", "a utopia")
+
+#pointers and addresses
+new_first_name = first_name
+puts new_first_name
+first_name = "Emmett"
+puts first_name
+puts new_first_name
+
+#escape sequence
+puts "my first name is \#{first_name}"
+puts 'he said "good bye"!'
