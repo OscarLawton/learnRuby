a9b34524ce475b92a34c817c2c65042140332a2c
Emmet <emmetlawton@gmail.com>
2019-12-23 04:26:31 +0000

    first video

---
+++ into.rb
@@ -0,0 +1,13 @@
+puts "hello world"
+
+question = "How are you today?"
+
+puts question
+
+def saySomething(thingToSay)
+  puts thingToSay
+end
+
+answer = "I'm well and hope you are too."
+
+saySomething(answer)
